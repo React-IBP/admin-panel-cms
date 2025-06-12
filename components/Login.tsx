e5585f5631bda786280dc2d0ff9192b4415aa972
ibalmaceda <sisdetcol@gmail.com>
2025-06-12 09:15:20 -0500

    up1

--- components/Login.tsx
+++ components/Login.tsx
@@ -138,7 +138,7 @@ const LoginForm = () => {
                                 <ButtonLoginGoogle toggleAuth={toggleAuth} />
                             </div>
                             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                Don’t have an account yet?  
                             </p>
                         </form>
                     </div>
